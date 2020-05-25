a2501a72a50f55e80ebace28f072dd901c672351
samyabanerjee <65707033+samyabanerjee@users.noreply.github.com>
2020-05-25 14:53:32 -0400

    ompiled

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,12 +5,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Web;
 
 namespace FrontDoorCDN.Pages
 {
+    [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        
 
         public IndexModel(ILogger<IndexModel> logger)
         {
